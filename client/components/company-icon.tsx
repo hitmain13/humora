a93b6ac03e8f3969bfc967ff3df33e797873ec9b
Fabio <hide.fabio123@gmail.com>
2025-07-02 01:02:23 -0300

    troca favicon

--- client/components/company-icon.tsx
+++ client/components/company-icon.tsx
@@ -3,9 +3,9 @@ import clsx from "clsx";
 export const CompanyIcon = ({ className }: { className?: string }) => {
   return (
     <img
-      src="https://cdn.builder.io/api/v1/image/assets%2F32890c9d769e420f993df6a7c6d5461f%2F146fc03c343146459ddb91594750a5ab?format=webp&width=800"
+      src="https://cdn.builder.io/api/v1/image/assets%2F32890c9d769e420f993df6a7c6d5461f%2Ff108ef6efd08483494cb6f873bacc205?format=webp&width=800"
       alt="Humora"
-      className={clsx("w-auto h-12 sm:w-12 sm:h-16", className)}
+      className={clsx("w-auto h-12 sm:h-16 ml-4 my-2", className)}
     />
   );
 };
